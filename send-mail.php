30b732fcb5c8e0bea3022359d5ffed931ea2c3d7
Oscar Perez <operezhernandez25@gmail.com>
2019-10-10 00:02:46 -0600

    upload server

--- send-mail.php
+++ send-mail.php
@@ -29,7 +29,7 @@
   if($responseKeys["success"]) {
     require 'vendor/autoload.php'; // If you're using Composer (recommended)
     // Comment out the above line if not using Composer
-    // require("<PATH TO>/sendgrid-php.php");
+    require("sendgrid-php/sendgrid-php.php");
     // If not using Composer, uncomment the above line and
     // download sendgrid-php.zip from the latest release here,
     // replacing <PATH TO> with the path to the sendgrid-php.php file,
